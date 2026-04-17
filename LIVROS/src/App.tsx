3743fa80d95a1533372dafea66d579eba7084c14
Stiven Dias <diasstiven2@gmail.com>
2026-04-16 23:05:58 -0100

    h

--- LIVROS/src/App.tsx
+++ LIVROS/src/App.tsx
@@ -4,7 +4,7 @@ import Sobre from './components/Sobre.tsx'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Livros from './components/Livros.tsx'
 import Footer from './components/Footer.tsx'
-import Home from './components/Home.tsx';
+import Home from './components/home.tsx';
 
 function App() {
   return (
