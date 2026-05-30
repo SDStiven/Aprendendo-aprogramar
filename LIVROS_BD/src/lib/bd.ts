049e7495cb34d0cce300ef3fcf97e8ae82716cf4
Stiven Dias <diasstiven2@gmail.com>
2026-05-29 23:40:11 -0100

    feat: implement user authentication flow with login and profile components, CORS support, and database configuration updates

--- LIVROS_BD/src/lib/bd.ts
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql2/promise';
-const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'servidor local',
-    database: 'bd_livros'
-})
-
-export default db
